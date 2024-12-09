0a32875fe4eaed3f27f1956ca86e886c1a560ff7
Harsh Hirawat <106510913+GreenHacker420@users.noreply.github.com>
2024-12-09 06:36:12 +0530

    upda

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myGrid");
+    if (x.className === "w3-row") {
+      x.className = "w3-row-padding";
+    } else { 
+      x.className = x.className.replace("w3-row-padding", "w3-row");
+    }
+  }
+  
